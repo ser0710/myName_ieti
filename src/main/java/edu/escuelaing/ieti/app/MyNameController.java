ac0093ff3f73043af86aca8313619041152ff02d
ser0710 <serroz2822@gmail.com>
2023-02-28 20:39:12 -0500

    dependencia

--- src/main/java/edu/escuelaing/ieti/app/MyNameController.java
+++ src/main/java/edu/escuelaing/ieti/app/MyNameController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api")
 public class MyNameController {
-    @GetMapping
+    @GetMapping("/")
     public String name(){
         return "Sergio Rozo";
     }
